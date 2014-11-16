f377cb668d3965c162f7ac05e212569cdbd2956a
Alexey V Gorshkov <animus@wayround.org>
2014-11-16 14:48:39 +0300

    added native styling

--- org/wayround/coolpassgen/MainApplet.java
+++ org/wayround/coolpassgen/MainApplet.java
@@ -6,6 +6,23 @@ public class MainApplet extends javax.swing.JApplet {
     public void init() {
 
         try {
+            javax.swing.UIManager.setLookAndFeel(
+                javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(
+                java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(
+                java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(
+                java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(
+                java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        try {
             javax.swing.SwingUtilities.invokeAndWait(
                 new org.wayround.coolpassgen.AppletRunnable(this) 
             );
